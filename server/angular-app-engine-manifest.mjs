fb2d76f0a583f76c1c0a6dd47faafea697cc9072
AXEL00000000 <161102260+AXEL00000000@users.noreply.github.com>
2025-07-17 16:26:03 -0600

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: 'C:/Program Files/Git/proyectAngular',
+  basePath: '/proyectAngular',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,34 +2,37 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'C:/Program Files/Git/proyectAngular/',
+  baseHref: '/proyectAngular/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/Program%20Files/Git/proyectAngular"
+    "route": "/proyectAngular"
   },
   {
     "renderMode": 2,
-    "route": "/Program%20Files/Git/proyectAngular/login"
+    "route": "/proyectAngular/login"
   },
   {
     "renderMode": 2,
-    "route": "/Program%20Files/Git/proyectAngular/tabledogs"
+    "route": "/proyectAngular/tabledogs"
   },
   {
     "renderMode": 2,
-    "route": "/Program%20Files/Git/proyectAngular/info-user"
+    "route": "/proyectAngular/info-user"
   },
   {
     "renderMode": 2,
-    "route": "/Program%20Files/Git/proyectAngular/user-dialog"
+    "route": "/proyectAngular/user-dialog"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23547, hash: '3d3c9a6ba91a5de1e1c95ac3135a9aa6c2f0310d0e02b2ef2f79df9d0fec9496', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17150, hash: 'a3c3e9fc2d000b42b64665e60afbc7491e85abf5aca9d757310d6fc308dfaf7a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23527, hash: 'a2458e09bdd11ae4d1eb118295515b71fb33c6269c1a93accb7cdce115f3b306', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17130, hash: 'd5cad3831adf8a265ee802e03bb3fe13520ae226b8944c72b4cecc8d53c1be1d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 105990, hash: 'a253962de7df6fb0796bf73400c5988bd82e31b784165fbd40840c3cc4e3461e', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 105990, hash: 'a253962de7df6fb0796bf73400c5988bd82e31b784165fbd40840c3cc4e3461e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'info-user/index.html': {size: 58446, hash: '9fce7a4defcc0d649da56d98b49f8ac900c413930841617c58b50240a364b930', text: () => import('./assets-chunks/info-user_index_html.mjs').then(m => m.default)},
     'styles-Z5UYKNDX.css': {size: 6934, hash: 'VxIyzD7LHG8', text: () => import('./assets-chunks/styles-Z5UYKNDX_css.mjs').then(m => m.default)}
   },
 };
